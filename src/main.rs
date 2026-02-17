3cd39e0d45aa3c0a4ed3b38d3e61a713e494e39c
俺不叫化咕龙 <1801943622@qq.com>
2026-02-17 15:37:41 +0800

    feat: use bufWriter to speed up

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{BufWriter, Write};
 
 use anyhow::Context;
 
@@ -8,17 +8,20 @@ const HEIGHT: usize = 9 * FACTOR;
 const MAX_COLOR_VALUE: usize = 255;
 
 fn main() -> anyhow::Result<()> {
-    let mut f = std::fs::File::create("output.ppm").context("create output.ppm")?;
+    for i in 0..60 {
+        let f = std::fs::File::create(format!("assets/output-{:02}.ppm", i))
+            .context("create output.ppm")?;
+        let mut writer = BufWriter::new(f);
 
-    writeln!(f, "P6").context("write ppm format")?;
-    writeln!(f, "{} {}", WIDTH, HEIGHT).context("write image's width & height")?;
-    writeln!(f, "{}", MAX_COLOR_VALUE).context("write max color value")?;
+        writeln!(writer, "P6").context("write ppm format")?;
+        writeln!(writer, "{} {}", WIDTH, HEIGHT).context("write image's width & height")?;
+        writeln!(writer, "{}", MAX_COLOR_VALUE).context("write max color value")?;
 
-    for h in 0..HEIGHT {
-        for w in 0..WIDTH {
-            f.write_all(&[0xff, 0, 0])?;
+        for h in 0..HEIGHT {
+            for w in 0..WIDTH {
+                writer.write_all(&[0xff, 0, 0])?;
+            }
         }
     }
-
     Ok(())
 }
